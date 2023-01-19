f28cde01ee3afd54bcdfeeff0e39e00133a7ed6b
alexokin <alex.okin@gmail.com>
2023-01-19 21:13:23 +0200

    commit

--- src/cmps/board-details.jsx
+++ src/cmps/board-details.jsx
@@ -1,55 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom'
-import { showErrorMsg } from '../services/event-bus.service.js'
-import { groupService } from '../services/group.service.local.js'
-import { GroupList } from './group-list.jsx'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { showErrorMsg } from "../services/event-bus.service.js";
+import { groupService } from "../services/group.service.local.js";
+import { GroupList } from "./group-list.jsx";
 import { FiPlus } from "react-icons/fi";
-import { GroupAdd } from './group-add.jsx'
-import { useSelector } from 'react-redux'
-import { setBoard, updateBoard } from '../store/board.actions.js'
+import { GroupAdd } from "./group-add.jsx";
+import { useSelector } from "react-redux";
+import { setBoard, updateBoard } from "../store/board.actions.js";
 import { BoardNavBar } from "./board-navbar";
 import { TaskDetails } from "./task-details";
 
-
 export function BoardDetails() {
-  const board = useSelector((storeState) => storeState.boardModule.currBoard)
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false)
-  const [addModalLoc, setAddModalLoc] = useState(null)
-  const { boardId, taskId } = useParams()
+  const board = useSelector((storeState) => storeState.boardModule.currBoard);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [addModalLoc, setAddModalLoc] = useState(null);
+  const { boardId, taskId } = useParams();
 
   useEffect(() => {
-    ; (async () => {
+    (async () => {
       try {
-        setBoard(boardId)
+        setBoard(boardId);
       } catch (err) {
-        showErrorMsg('Cannot load board', err)
+        showErrorMsg("Cannot load board", err);
       }
-    })()
-  }, [])
+    })();
+  }, []);
 
   function onToggleAddModal(ev) {
-    const BoundingClientRect = ev?.target.getBoundingClientRect()
+    const BoundingClientRect = ev?.target.getBoundingClientRect();
     const addModalLocToSet = {
-      left: `${BoundingClientRect?.left}px`
-    }
-    setIsAddModalOpen(prevState => !prevState)
-    setAddModalLoc(addModalLocToSet)
+      left: `${BoundingClientRect?.left}px`,
+    };
+    setIsAddModalOpen((prevState) => !prevState);
+    setAddModalLoc(addModalLocToSet);
   }
 
   return (
-    <div style={board?.style} className='board-details'>
+    <div style={board?.style} className="board-details">
       {/* <BoardNavBar /> */}
       {/* <BoardHeader />
       <SideNavBar /> */}
-      {board && <GroupList board={board} groups={board.groups} onRemoveGroup={onRemoveGroup} />}
-      <div className='btn-open-addGroup' onClick={(event) => onToggleAddModal(event)}><FiPlus />Add another list</div>
-      {isAddModalOpen && <GroupAdd board={board}  onToggleAddModal={onToggleAddModal} addModalLoc={addModalLoc} />}
+
+      {board && <GroupList board={board} groups={board.groups} />}
+      <div
+        className="btn-open-addGroup"
+        onClick={(event) => onToggleAddModal(event)}
+      >
+        <FiPlus />
+        Add another list
+      </div>
+      {isAddModalOpen && (
+        <GroupAdd
+          board={board}
+          onToggleAddModal={onToggleAddModal}
+          addModalLoc={addModalLoc}
+        />
+      )}
       {taskId && <TaskDetails />}
     </div>
-  { board && <GroupList board={board} groups={board.groups} /> }
-  <div className='btn-open-addGroup' onClick={(event) => onToggleAddModal(event)}><FiPlus />Add another list</div>
-  { isAddModalOpen && <GroupAdd board={board} onToggleAddModal={onToggleAddModal} addModalLoc={addModalLoc} /> }
-    </div >
-  )
+  );
 }
