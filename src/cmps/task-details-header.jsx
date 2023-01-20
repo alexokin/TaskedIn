542964a2e3c1cd153689bbe73c53e5ec9554d7e4
alexokin <alex.okin@gmail.com>
2023-01-20 12:56:05 +0200

    first version of task details is done.

--- src/cmps/task-details-header.jsx
+++ src/cmps/task-details-header.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { GrCreditCard } from "react-icons/gr";
+import { groupService } from "../services/group.service.local";
 
-export function TaskDetailsHeader({ taskId, groupId }) {
+
+export function TaskDetailsHeader({ task, groupId, groupTitle }) {
+  // const [groupTitle, setgroupTitle] = useState(second)
+
+  
+
   return (
     <section className="task-details-header">
       <input type="text" />
       <div className="sub-title">
         in list &nbsp;
-        <span className="group-title">{groupId}</span>
+        <span className="task-group-title">{groupTitle}</span>
       </div>
       <GrCreditCard className="header-icon" />
     </section>
--- src/cmps/task-details.jsx
+++ src/cmps/task-details.jsx
@@ -1,47 +1,55 @@
 import React, { Fragment } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useSelector } from 'react-redux'
-import { IoCloseOutline } from 'react-icons/io5'
+import { useSelector } from "react-redux";
+import { IoCloseOutline } from "react-icons/io5";
 import { TaskDetailsHeader } from "./task-details-header";
 import { TaskDetailsSidebar } from "./task-details-sidebar";
 import { TaskDescription } from "./task-description";
 import { TaskDetailsActivities } from "./task-details-activity";
-
+import { groupService } from "../services/group.service.local";
 
 export function TaskDetails() {
-  const board = useSelector((state) => state.boardModule.board)
+  const board = useSelector((storeState) => storeState.boardModule.currBoard);
   const navigate = useNavigate();
-  const { groupId, taskId } = useParams();
+  const { boardId, groupId, taskId } = useParams();
 
-  console.log(groupId, taskId);
+  const group = board.groups.find((group) => group._id === groupId)
+  let task = group.tasks.find((task) => task._id === taskId)
+
+ 
 
   function backToBoard(ev) {
-    ev.stopPropagation()
-    navigate(-1)
+    ev.stopPropagation();
+    navigate(-1);
   }
+
+  
 
   return (
     <Fragment>
-    <section className="screen">
-      <div onClick={backToBoard} className="backdrop"></div>
-      <section className="task-details-container" >
-        <section  className="task-details" onClick={(ev) => ev.stopPropagation()}>
-          <button className='close-task-details'  onClick={backToBoard}><IoCloseOutline /></button>
+      <section className="screen">
+        <div onClick={backToBoard} className="backdrop"></div>
+        <section className="task-details-container">
+          <section
+            className="task-details"
+            onClick={(ev) => ev.stopPropagation()}
+          >
+            <button className="close-task-details" onClick={backToBoard}>
+              <IoCloseOutline />
+            </button>
 
-          <TaskDetailsHeader />
-          <div className="task-body">
-            <section className="task-content">
-              {/* <TaskDetailsOverview /> */}
-              <TaskDescription/>
-              <TaskDetailsActivities />
-         
-            </section>
-            <TaskDetailsSidebar />
-          </div>
+            <TaskDetailsHeader task={task} groupId={groupId} groupTitle={group.title} />
+            <div className="task-body">
+              <section className="task-content">
+                {/* <TaskDetailsOverview /> */}
+                <TaskDescription />
+                <TaskDetailsActivities />
+              </section>
+              <TaskDetailsSidebar />
+            </div>
+          </section>
         </section>
       </section>
-
-    </section>
-  </Fragment>
+    </Fragment>
   );
 }
