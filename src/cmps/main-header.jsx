aa4aee4605624eaed8410046436b78aa2c3b1493
eli-shallev <eli.shallev50@gmail.com>
2023-01-20 20:24:01 +0200

    main header added

--- src/cmps/main-header.jsx
+++ src/cmps/main-header.jsx
@@ -1,9 +1,41 @@
+import { InputAdornment, TextField } from '@mui/material'
 import React from 'react'
+import { BiSearch } from "react-icons/bi"
+import { AiOutlineDown } from "react-icons/ai"
 
 export function MainHeader() {
   return (
     <div className='main-header'>
-     <a href="/workspace">blabla</a>
+      <div className='left-container'>
+        <a className='logo' href="/workspace">Trello</a>
+        <div className='btn-recent'>
+          <span>Recent</span>
+          <AiOutlineDown />
+        </div>
+        <div className='btn-starred'>
+          <span>Starred</span>
+          <AiOutlineDown />
+        </div>
+        <div className='btn-create'>Create</div>
+      </div>
+      <div className='right-container'>
+        <TextField
+          id="title"
+          size="small"
+          placeholder='Search'
+          //onChange={handleChange}
+          //value={filterByToEdit.title}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <BiSearch />
+              </InputAdornment>
+            ),
+          }}
+          variant="outlined"
+        />
+        <img width='40px' src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="" />
+      </div>
     </div>
   )
 }
