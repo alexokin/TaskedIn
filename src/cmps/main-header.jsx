85091c34e047842731e0a532d08c7d27ad4fb85a
eli-shallev <eli.shallev50@gmail.com>
2023-01-21 19:27:36 +0200

    board search from main header added

--- src/cmps/main-header.jsx
+++ src/cmps/main-header.jsx
@@ -1,13 +1,13 @@
-import { InputAdornment, TextField } from '@mui/material'
 import React, { useState } from 'react'
 import { BiSearch } from "react-icons/bi"
 import { AiOutlineDown } from "react-icons/ai"
 import { BoardStarredList } from './board-starred-list'
 import { BoardAdd } from './board-add'
-import { useNavigate } from 'react-router'
+import { BoardSearchList } from './board-search-list'
 
 export function MainHeader() {
   const [isStarredListOpen, setIsStarredListOpen] = useState(false)
+  const [isSearchListOpen, setIsSearchListOpen] = useState(false)
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false)
   const [modalLoc, setmodalLoc] = useState(null)
 
@@ -19,6 +19,16 @@ export function MainHeader() {
       top: `${BoundingClientRect?.bottom}px`
     }
     setIsStarredListOpen(prevState => !prevState)
+    setmodalLoc(modalLocToSet)
+  }
+
+  function toggleSearchList(ev) {
+    const BoundingClientRect = ev?.target.getBoundingClientRect()
+    const modalLocToSet = {
+      left: `${BoundingClientRect?.right -500 }px`,
+      //top: `${BoundingClientRect?.bottom}px`,
+    }
+    setIsSearchListOpen(prevState => !prevState)
     setmodalLoc(modalLocToSet)
   }
 
@@ -47,16 +57,15 @@ export function MainHeader() {
         <div className='btn-create' onClick={(event) => onToggleCreateBoardModal(event)}>Create</div>
       </div>
       <div className='right-container'>
-        <div className='search-container'>
-          <BiSearch />
-          <input type="text"
-            placeholder='Search' />
+        <div className='search-container' onClick={(event) => toggleSearchList(event)}>
+          <BiSearch /> Search
         </div>
         <img width='30px' src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="" />
       </div>
 
       {isStarredListOpen && <BoardStarredList toggleStarredList={toggleStarredList} modalLoc={modalLoc} />}
       {isCreateModalOpen && <BoardAdd addModalLoc={modalLoc} onToggleAddBoardModal={onToggleCreateBoardModal} fromHeader={true} />}
+      {isSearchListOpen && <BoardSearchList toggleSearchList={toggleSearchList} modalLoc={modalLoc} />}
     </div>
   )
 }
