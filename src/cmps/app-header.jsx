391d4783fe5ffac12edfebec7ccf3a992a82eb94
Yossef Nahari <you@example.com>
2023-01-22 01:49:00 +0200

    Update UI of Home page

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,19 +1,14 @@
-import { Link, NavLink } from 'react-router-dom'
-import {useSelector} from 'react-redux'
-import routes from '../routes'
-import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
-import { login, logout, signup } from '../store/user.actions.js'
-import { LoginSignup } from '../pages/login-signup.jsx'
-
 export function AppHeader() {
 
     return (
-        <header className="header-main">
-                <a href="" className='a-logo'><img src={require(`../assets/img/trello-logo.png`)} alt="trello logo" className="header-nav-logo"/></a>
-                <div className="home-page-nav-btns">
-                    <a href="" className="login-btn">Log in</a>
-                    <a href="/workspace" className="try-trello-btn">Get Trello for free</a>
-                </div>
+        <header className="header-main full">
+            <div className="header-sub">
+                <a href="/" className='a-logo'><img src={require(`../assets/img/trello-logo.png`)} alt="trello logo" className="header-nav-logo"/></a>
+                <div className="header-nav-btns">
+                    <a href="" className="a-login fs18">Log in</a>
+                    <a href="/workspace" className="a-try fs18">Get Trello for free</a>
+                    </div>
+                    </div>
         </header>
     )
 }
