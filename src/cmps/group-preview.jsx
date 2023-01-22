942d97f361a321c533462c9f6ac6e3f9630a81de
eli-shallev <eli.shallev50@gmail.com>
2023-01-22 16:46:29 +0200

    board details pixel perfect improved

--- src/cmps/group-preview.jsx
+++ src/cmps/group-preview.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { showErrorMsg } from "../services/event-bus.service.js";
 import { taskService } from "../services/task.service.local.js";
+import { AddTask } from "./add-task.jsx";
 import { GroupTitle } from "./group-title.jsx";
 import { TaskList } from "./task-list.jsx";
+import { FiPlus } from "react-icons/fi";
 
 export function GroupPreview({ group, board }) {
 
   const filter = useSelector((storeState) => storeState.systemModule.filter)
   const [isFiltered, setIsFiltered] = useState(false)
-
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false)
   useEffect(() => {
 
     // const regex = new RegExp(filter.keyword, 'i')
@@ -18,10 +20,18 @@ export function GroupPreview({ group, board }) {
     // setIsFiltered(true)
   }, [filter])
 
+  function onToggleAddModal() {
+    setIsAddModalOpen(prevState => !prevState)
+  }
+
   return (
     <div className="group-preview">
       <GroupTitle group={group} board={board} />
-      {!isFiltered && <TaskList groupTasks={group.tasks} board={board} groupId={group._id} />}
+      {group.tasks.length!==0 && <TaskList groupTasks={group.tasks} board={board} groupId={group._id} />}
+      <div className="add-container">
+        {!isAddModalOpen && <div onClick={onToggleAddModal} className="btn-open-add-task"><div className="plus"><FiPlus /></div>Add a card</div>}
+        {isAddModalOpen && <AddTask onToggleAddModal={onToggleAddModal} tasks={group.tasks} board={board} groupId={group._id} />}
+      </div>
     </div>
   )
 }
--- src/cmps/task-list.jsx
+++ src/cmps/task-list.jsx
@@ -5,23 +5,17 @@ import { groupService } from "../services/group.service.local";
 import { utilService } from "../services/util.service";
 import { AddTask } from "./add-task.jsx";
 import { taskService } from "../services/task.service.local.js";
-import { addTask } from "../store/actions/task.actions";
-import { FiPlus } from "react-icons/fi";
 
 export function TaskList({ groupTasks, groupId, board }) {
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false)
+  
 
-  function onToggleAddModal() {
-    setIsAddModalOpen(prevState => !prevState)
-  }
+  
 
   return (
     <div className="task-list">
       {groupTasks.map((task, index) => (
         <TaskPreview board={board} key={task._id} task={task} groupId={groupId} />
       ))}
-      {!isAddModalOpen && <div onClick={onToggleAddModal} className="btn-open-add-task"><FiPlus />Add a card</div>}
-      {isAddModalOpen && <AddTask onToggleAddModal={onToggleAddModal} tasks={groupTasks} board={board} groupId={groupId} />}
     </div>
   );
 }
