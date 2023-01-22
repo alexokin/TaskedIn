b065e61968280909b40e980da85f9e1131d2b468
eli-shallev <eli.shallev50@gmail.com>
2023-01-22 11:16:13 +0200

    hide main header on home page

--- src/cmps/main-header.jsx
+++ src/cmps/main-header.jsx
@@ -4,12 +4,20 @@ import { AiOutlineDown } from "react-icons/ai"
 import { BoardStarredList } from './board-starred-list'
 import { BoardAdd } from './board-add'
 import { BoardSearchList } from './board-search-list'
+import { useEffect } from 'react'
 
 export function MainHeader() {
   const [isStarredListOpen, setIsStarredListOpen] = useState(false)
   const [isSearchListOpen, setIsSearchListOpen] = useState(false)
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false)
   const [modalLoc, setmodalLoc] = useState(null)
+  const [hideHeader, setHideHeader] = useState(false)
+
+  useEffect(() => {
+    if (window.location.href === 'http://localhost:3000/') {
+      setHideHeader(true)
+    }
+  }, [])
 
 
   function toggleStarredList(ev) {
@@ -25,7 +33,7 @@ export function MainHeader() {
   function toggleSearchList(ev) {
     const BoundingClientRect = ev?.target.getBoundingClientRect()
     const modalLocToSet = {
-      left: `${BoundingClientRect?.right -500 }px`,
+      left: `${BoundingClientRect?.right - 500}px`,
       //top: `${BoundingClientRect?.bottom}px`,
     }
     setIsSearchListOpen(prevState => !prevState)
@@ -43,29 +51,31 @@ export function MainHeader() {
   }
 
   return (
-    <div className='main-header'>
-      <div className='left-container'>
-        <a className='logo' href="/workspace">Trello</a>
-        <div className='btn-recent'>
-          Recent
-          <AiOutlineDown />
+    <div>
+      {!hideHeader && <div className='main-header'>
+        <div className='left-container'>
+          <a className='logo' href="/workspace">Trello</a>
+          <div className='btn-recent'>
+            Recent
+            <AiOutlineDown />
+          </div>
+          <div className='btn-starred' onClick={(event) => toggleStarredList(event)}>
+            Starred
+            <AiOutlineDown />
+          </div>
+          <div className='btn-create' onClick={(event) => onToggleCreateBoardModal(event)}>Create</div>
         </div>
-        <div className='btn-starred' onClick={(event) => toggleStarredList(event)}>
-          Starred
-          <AiOutlineDown />
+        <div className='right-container'>
+          <div className='search-container' onClick={(event) => toggleSearchList(event)}>
+            <BiSearch /> Search
+          </div>
+          <img width='30px' src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="" />
         </div>
-        <div className='btn-create' onClick={(event) => onToggleCreateBoardModal(event)}>Create</div>
-      </div>
-      <div className='right-container'>
-        <div className='search-container' onClick={(event) => toggleSearchList(event)}>
-          <BiSearch /> Search
-        </div>
-        <img width='30px' src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="" />
-      </div>
 
-      {isStarredListOpen && <BoardStarredList toggleStarredList={toggleStarredList} modalLoc={modalLoc} />}
-      {isCreateModalOpen && <BoardAdd addModalLoc={modalLoc} onToggleAddBoardModal={onToggleCreateBoardModal} fromHeader={true} />}
-      {isSearchListOpen && <BoardSearchList toggleSearchList={toggleSearchList} modalLoc={modalLoc} />}
+        {isStarredListOpen && <BoardStarredList toggleStarredList={toggleStarredList} modalLoc={modalLoc} />}
+        {isCreateModalOpen && <BoardAdd addModalLoc={modalLoc} onToggleAddBoardModal={onToggleCreateBoardModal} fromHeader={true} />}
+        {isSearchListOpen && <BoardSearchList toggleSearchList={toggleSearchList} modalLoc={modalLoc} />}
+      </div>}
     </div>
   )
 }
