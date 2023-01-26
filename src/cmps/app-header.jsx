f5c26634a87c83b0b9a4d409cd940dcbfc4ed59a
Yossef Nahari <you@example.com>
2023-01-26 11:36:16 +0200

    ui fixes

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -5,7 +5,7 @@ export function AppHeader() {
             <div className="header-sub">
                 <a href="/" className='a-logo'><img src={require(`../assets/img/new-logo.png`)} alt="trello logo" className="header-nav-logo"/></a>
                 <div className="header-nav-btns">
-                    <a href="" className="a-login fs18-5">Log in</a>
+                    {/* <a href="" className="a-login fs18-5">Log in</a> */}
                     <a href="/workspace" className="a-try fs18">Get TaskedIn now!</a>
                     </div>
                     </div>
